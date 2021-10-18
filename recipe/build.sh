9c376a62fe09d5a41f990d41c1a76e4fc69e976a
akrherz <akrherz@iastate.edu>
2021-10-18 11:09:26 -0500

    try a different command to build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ rm -rf g2clib-* wgrib2/{fnlist,Gctpc,gctpc_ll2xy,new_grid_lambertc}.[ch]
 cp $RECIPE_DIR/config.h wgrib2/config.h
 
 pushd wgrib2
-make fnlist.c
+./function.sh
 make
 
 cp wgrib2 $PREFIX/bin
