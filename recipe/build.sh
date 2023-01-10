6655508b90f04a9b95719d31b96956b295814784
akrherz <akrherz@iastate.edu>
2023-01-10 06:39:13 -0600

    add mysql include to build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LDFLAGS="-lgrib2c -ljasper -lnetcdf -lpng -lmysqlclient -lz -lm -fopenmp $LDFLAGS"
-export CFLAGS="-I.. -fopenmp $CFLAGS"
+export CFLAGS="-I.. -I$PREFIX/include/mysql -fopenmp $CFLAGS"
 
 rm -rf g2clib-* wgrib2/{fnlist,Gctpc,gctpc_ll2xy,new_grid_lambertc}.[ch]
 cp $RECIPE_DIR/config.h wgrib2/config.h
